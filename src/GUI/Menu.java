2d67f2f2093d2879dd799ae8e60510c02ad9cb80
Joel Esteban Diaz Arévalo <jodiaza@unal.edu.co>
2019-07-07 15:55:40 -0500

    Boceto menu agregado

--- src/GUI/Menu.java
+++ src/GUI/Menu.java
@@ -1,12 +1,29 @@
 package GUI;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class Menu extends Pantalla{
 	
-	public void mostrarMenu() {
-		
-	}
+    static int ancho = 500;
+    static int alto = 500;
+        
+    public static void mostrarMenu() {
+        JFrame menu = new JFrame("Menu");
+        menu.setSize(ancho, alto);
+        menu.setVisible(true);
+        JPanel panel = new JPanel();
+        menu.add(panel);
+        JButton ajustes = new JButton("Ajustes");
+        JButton tutorial = new JButton("Tutorial");
+        JButton mapa = new JButton("Mapa");
+        panel.add(ajustes);
+        panel.add(tutorial);
+        panel.add(mapa);
+    }
 	
-	public void  acceder() {
+    public void  acceder() {
 		
-	}
+    }
 }
--- src/logicaDeNegocio/Logica.java
+++ src/logicaDeNegocio/Logica.java
@@ -1,9 +1,10 @@
 package logicaDeNegocio;
+import GUI.Menu;
 
 public class Logica {
 
     public static void main(String[] args) {
-        
+        Menu.mostrarMenu();
     }
    
     public static void  ejecutar() {
